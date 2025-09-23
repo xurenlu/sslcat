a3c127c01de69ba48d3991cfb3cf22b79673f052
rocky.x <guy@some.im>
2025-09-23 23:14:59 +0800

    添加通知管理菜单项
    - 在侧边栏中添加通知管理菜单项
    - 添加通知相关的多语言翻译
    - 完善通知管理功能的入口
    - 支持中文、英文、西班牙语、法语、日语、俄语

--- internal/web/api_runners.go
+++ internal/web/api_runners.go
@@ -715,12 +715,12 @@ func (s *Server) handleGitServer(w http.ResponseWriter, r *http.Request) {
 	gitApps := s.gitServer.ListApps()
 
 	data := map[string]interface{}{
-		"AdminPrefix": s.config.AdminPrefix,
-		"GitApps":     gitApps,
-		"GitEnabled":  s.config.Runners.Git.Enabled,
-		"Title": s.translator.T("git_deploy_server.title"),
+		"AdminPrefix":         s.config.AdminPrefix,
+		"GitApps":             gitApps,
+		"GitEnabled":          s.config.Runners.Git.Enabled,
+		"Title":               s.translator.T("git_deploy_server.title"),
 		"FunctionDescription": s.translator.T("git_deploy_server.function_description"),
-		"Description": s.translator.T("git_deploy_server.description"),
+		"Description":         s.translator.T("git_deploy_server.description"),
 	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
